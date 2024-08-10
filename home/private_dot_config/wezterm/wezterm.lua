4b4c42a7e241d7b5930d620828a36d255c963744
Jay Llacuna <626146+jllacuna@users.noreply.github.com>
2024-08-10 15:16:00 -0700

    Added script to install Hack font (not patched with Nerd Fonts). Changed wezterm to use regular Hack font. Adjusted Oh My Posh icons. Fixed scripts that clean up previous installs

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -4,12 +4,12 @@ local config = wezterm.config_builder()
 
 config.enable_tab_bar = false -- Use tmux for tabs
 
-config.font = wezterm.font 'Hack Nerd Font'
+config.font = wezterm.font 'Hack'
 config.font_size = 16.0
 
 config.color_scheme = 'Jellybeans'
 
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.95
 
 -- https://github.com/wez/wezterm/issues/5612
 config.front_end = "WebGpu"
