b5eb513d14c253f612e9bfe6e8d6d428d4dd7a2f
Jay Llacuna <626146+jllacuna@users.noreply.github.com>
2024-08-15 13:50:45 -0700

    Updated wezterm to copy/paste with right mouse button

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require 'wezterm'
 
+local act = wezterm.action
 local config = wezterm.config_builder()
 
 config.enable_tab_bar = false -- Use tmux for tabs
@@ -10,6 +11,23 @@ config.font_size = 16.0
 config.color_scheme = 'Jellybeans'
 
 config.window_background_opacity = 0.95
+
+-- Copy/paste with right mouse button
+config.mouse_bindings = {
+  {
+    event = { Down = { streak = 1, button = "Right" } },
+    mods = "NONE",
+    action = wezterm.action_callback(function(window, pane)
+      local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+      if has_selection then
+        window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+        window:perform_action(act.ClearSelection, pane)
+      else
+        window:perform_action(act.PasteFrom("Clipboard"), pane)
+      end
+    end),
+  },
+}
 
 -- https://github.com/wez/wezterm/issues/5612
 config.front_end = "WebGpu"
