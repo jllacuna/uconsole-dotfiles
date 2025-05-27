718dd40708ca250fbe463400d8273fa470658e94
Jay Llacuna <626146+jllacuna@users.noreply.github.com>
2025-05-27 12:40:16 -0700

    Styled cpu and mem displays in tmux

--- home/private_dot_config/tmux/executable_cpu.sh
+++ home/private_dot_config/tmux/executable_cpu.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-grep 'cpu' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf("%.2f%%", usage)}'
+
+grep 'cpu' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf("#[fg=%s]%.2f%%", usage > 80 ? "#aa092c" : "#12a13a", usage)}'
--- home/private_dot_config/tmux/executable_mem.sh
+++ home/private_dot_config/tmux/executable_mem.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-free | grep Mem | awk '{printf("%.2f%%", $3/$2 * 100.0)}'
+free | grep Mem | awk '{usage=$3/$2*100} END {printf("#[fg=%s]%.2f%%", usage > 80 ? "#aa092c" : "#12a13a", usage)}'
