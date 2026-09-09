f3fae4722d12e20ea20447de01c6b2dddab9260f
Jay Llacuna <626146+jllacuna@users.noreply.github.com>
2026-09-09 01:23:37 -0700

    Enabled project-local nvim config

--- home/private_dot_config/nvim/lua/user/options.lua
+++ home/private_dot_config/nvim/lua/user/options.lua
@@ -6,6 +6,7 @@ local options = {
   conceallevel   = 0,                                    -- so that `` is visible in markdown files
   cursorline     = false,                                -- highlight the current line
   expandtab      = true,                                 -- convert tabs to spaces
+  exrc           = true,                                 -- load project-local config
   fileencoding   = "utf-8",                              -- the encoding written to a file
   hlsearch       = true,                                 -- highlight all matches on previous search pattern
   ignorecase     = true,                                 -- ignore case in search patterns
